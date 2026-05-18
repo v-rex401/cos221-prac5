708c39033fbb1d5f4d3fdb791bad3b9ba976ae60
Vashti <vashtipillay104@gmail.com>
2026-05-18 23:28:59 +0200

    Added custom dashboard elements

--- pages/agency/agency_dashboard.php
+++ pages/agency/agency_dashboard.php
@@ -13,7 +13,7 @@
     //redirectIfNotAgency();
 
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,21 +21,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Agency Dashboard</title>
         <link rel="stylesheet" href="../../css/style.css">
-        //TODO: Change css file to use percentages insteea of pixels
-        <link rel="stylesheet" href="../../css/dashboard.css"> 
+        <!-- TODO: Change css file to use percentages insteea of pixels -->
+         <link rel="stylesheet" href="../../css/dashboard.css">
     </head>
     <body>
         <p>you are now in agency_dashboard</p>
         <div class="sidepanel"> 
             <h1>Welcome, John Doe</h1> 
          </div> 
-         <div class="mainBoard"> 
-            <div class="card"> <button id="createPackageBtn">Create Package</button></div>
-            <div class="card"> <button id="editPackageBtn">Edit</button> </div> 
-            <div class="card"> <button id="deletePackageBtn"> Delete </button> </div> 
+         <div id="mainBoard"> 
+            <!-- <div class="card"> <button id="createPackageBtn">Create Package</button></div> -->
+            
          </div> 
         <a href="../traveller/logout.php">Logout</a>
 
-        <script>"../../js/agencyDashboard.js"</script> 
+        <script src = "../../js/agencyDashboard.js"></script> 
     </body>
 </html>
