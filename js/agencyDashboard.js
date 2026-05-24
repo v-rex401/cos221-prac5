44adae5dfe6a63c6ca9a22decdc19f89ddde6cff
Vashti <vashtipillay104@gmail.com>
2026-05-24 14:35:04 +0200

    Delete package functionality JS

--- js/agencyDashboard.js
+++ js/agencyDashboard.js
@@ -7,4 +7,25 @@ window.onload = function () {
   deletePackage();
 };
 
-function deletePackage(packageId) {}
+function deletePackage(packageId) {
+  if (!confirm("Are you sure you want to delete this package?")) return;
+
+  fetch("../../includes/agency_dashboard_queries.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      type: "deletePackage",
+      package_id: packageId,
+      agency_id: AGENCY_ID,
+    }),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      if (response.success) {
+        location.reload();
+      } else {
+        alert("Error: " + response.message);
+      }
+    })
+    .catch((err) => console.error("Error deleting package:", err));
+}
